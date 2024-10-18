e88663703b513805ffa9d8e8823ceb5bd34bfe12
osman <miskhodzh@osman.myguest.virtualbox.org>
2024-10-18 23:12:31 +0300

    работа с текстом

--- main.py
+++ main.py
@@ -3,35 +3,26 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.properties import ObjectProperty, StringProperty
 
-class MyApp(App):
+
+class MainWidget(BoxLayout):
+
+    input_text = ObjectProperty(None)
+    label_text = StringProperty("")
+
+    def save_text_input(self):
+        text = self.input_text.text
+        with open("output.txt", 'w') as f:
+            f.write(text)
+    
+    def show_text(self):
+        with open("output.txt", "r") as f:
+            self.label_text = f.read()
+
+class TimerApp(App):
     def build(self):
-        # Изначально фон белый
-        Window.clearcolor = (1, 1, 1, 1)
-
-        # Основной контейнер - вертикальная компоновка
-        self.layout = BoxLayout(orientation='vertical', padding=20)
-
-        # Текст "Привет, меня зовут Осман!"
-        self.label = Label(text="Привет, меня зовут Осман!", font_size='24sp', color=(0, 0, 0, 1))
-        self.layout.add_widget(self.label)
-
-        # Кнопка "Нажми на меня!!!"
-        self.button = Button(text="Нажми на меня!!!", font_size='24sp', size_hint=(1, 0.2))
-        self.button.bind(on_press=self.change_color)  # Привязываем обработчик событий
-        self.layout.add_widget(self.button)
-
-        return self.layout
-
-    def change_color(self, instance):
-        # Меняем цвет фона и текста при нажатии
-        if Window.clearcolor == [1, 1, 1, 1]:  # Если фон белый
-            Window.clearcolor = (0, 0, 0, 1)  # Меняем на черный
-            self.label.color = (1, 1, 1, 1)   # Текст белый
-        else:
-            Window.clearcolor = (1, 1, 1, 1)  # Меняем на белый
-            self.label.color = (0, 0, 0, 1)   # Текст черный
-
+        return MainWidget()
 
 if __name__ == "__main__":
-    MyApp().run()
+    TimerApp().run()
